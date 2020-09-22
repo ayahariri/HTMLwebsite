406572eb77eb85fa72b1ca65fbcf4280df38e056
ayahariri <ayahariri899@hotmail.com>
2020-09-22 19:44:18 +0300

    new

---
+++ js/myscript.js
@@ -0,0 +1,49 @@
+alert ('Weclome to my Blog');
+
+var name = prompt ('Please enter your name');
+
+console.log(traveled);
+var traveled = prompt('Have you ever traveled?');
+//var greeting;
+//console.log(greeting);
+
+if (traveled = No) {
+greeting = 'Explore my world!';
+} else if (traveled = Yes) {
+    greeting = 'Welcome to my World!';
+}
+
+var age = 10
+age = age + 10
+console.log(age);
+
+document.write(`<h1>${greeting}</h1>`);
+
+
+//var today = new Date();
+
+console.log(today);
+var hourNow = prompt('please enter an hour from 0-23')
+var greeting;
+console.log(greeting);
+
+if (hourNow > 18 && hourNow <= 23) {
+    greeting = 'Good evening, Class!';
+  } else if (hourNow > 12 && hourNow <=18) {
+    greeting = 'Good afternoon, Class!';
+  } else if (hourNow >= 0 && hourNow <=12) {
+    greeting = 'Good morning!';
+  } else {
+    greeting = 'Something went wrong!';
+  }
+
+var age = 10
+age = age + 10
+console.log(age);
+
+var fname = 'aya ';
+var lname = 'hariri';
+
+console.log(fname+lname);
+
+document.write('<h3>'+greeting+'</h3>');
